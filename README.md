60a58be603e3d31f645db5cfa66919c7c2a65902
sanja_s <sanja100v@gmail.com>
2023-12-23 19:04:45 +0300

    upd:

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # 21_bot
 
+# Contents
+
+## Installation
+
+### Prerequisites
+
+### Usage
+1. source .env  -> After that, run the source .env command to read the environment variables from the .env file. 
+
+### TODO
+
+### help
 https://www.freecodecamp.org/news/how-to-create-a-telegram-bot-using-python/
-
-
--- bot.py
+++ bot.py
@@ -2,4 +2,43 @@ import os
 
 import telebot
 
-BOT_TOKEN = os.environ("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+# print(BOT_TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет, епт")
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
